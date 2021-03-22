b6b4362bb91abf4e198da6983368946b93c8a45a
Ryan <leeyuenhigh@gmail.com>
2021-03-22 01:24:33 -0400

    Started working on dummy info and arrangement of dummy info

---
+++ src/dummyStore.js
@@ -0,0 +1,79 @@
+export default {
+  "familes": [
+    {
+      "id" : 1,
+      "admin":"Bob",
+      "members":["Bob","Jack","Jill"],
+      "code-2-join":"aJk3T"
+    },
+    {
+      "id" : 2,
+      "admin":"Mary",
+      "members":["Mary","John","Amy"],
+      "code-2-join":"bJk3T"
+    },
+  ],
+  "people":[
+    {
+      "id":1,
+      "name":"Bob",
+      "chores":[
+        "Walk the dog",
+        "Take Jack to school",
+        "Go to the PO box"
+      ],
+      "level":1,
+      "xp-till-level-up":100,
+    },
+    {
+      "id":2,
+      "name":"Jack",
+      "chores":[
+        "Buy groceries",
+        "Water the plants"
+      ],
+      "level":1,
+      "xp-till-level-up":100,
+    },
+    {
+      "id":3,
+      "name":"Jill",
+      "chores":[
+        "Do the laundry",
+        "Take out the trash"
+      ],
+      "level":1,
+      "xp-till-level-up":100,
+    },
+    {
+      "id":4,
+      "name":"Mary",
+      "chores":[
+        "Do the laundry",
+        "Take out the trash"
+      ],
+      "level":1,
+      "xp-till-level-up":100,
+    },
+    {
+      "id":5,
+      "name":"John",
+      "chores":[
+        "Feed the fish",
+        "Vacuum the carpet"
+      ],
+      "level":1,
+      "xp-till-level-up":100,
+    },
+    {
+      "id":6,
+      "name":"Amy",
+      "chores":[
+        "Water the plants",
+        "Clean the fridge"
+      ],
+      "level":1,
+      "xp-till-level-up":100,
+    },
+  ]
+}
