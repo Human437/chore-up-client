2de8ad0805a59b766397ecfc39558ac5ca9e0e38
Ryan <leeyuenhigh@gmail.com>
2021-03-23 17:37:44 -0400

    Added routes for likely paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import ChoreUpContext from "./choreUpContext";
+import CreateAChore from "./create-a-chore/create-a-chore";
+import Management from "./management/management";
+import MyChores from "./my-chores/my-chores";
+import SignIn from "./sign-in/sign-in";
+import SignUp from "./sign-up/sign-up";
+import NavBar from './navbar/navbar';
+import HomePage from './home-page/home-page'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <ChoreUpContext.Provider value={{}}>
+        <main>
+          <Route path="/" component={NavBar} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/my-chores" component={MyChores} />
+          <Route path="/management" component={Management} />
+          <Route path="/create-a-chore" component={CreateAChore} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+        </main>
+      </ChoreUpContext.Provider>
+    );
+  }
 }
-
-export default App;
