6a53afaa3e6a79c16e42458a8fbbda47478fe994
Ryan <leeyuenhigh@gmail.com>
2021-03-22 20:52:16 -0400

    Added layout for sign in page

---
+++ src/sign-in/sign-in.js
@@ -0,0 +1,136 @@
+import React from "react";
+import "./sign-in.css";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+import ValidationError from "./../validationError";
+import config from "./../config";
+import { Link } from "react-router-dom";
+
+export default class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: {
+        value: "",
+        touched: false,
+      },
+      password: {
+        value: "",
+        touched: false,
+      },
+      isEmailInDb: true,
+      isPasswordCorrect: true,
+    };
+  }
+
+  updateEmail(email) {
+    this.setState({
+      email: { value: email, touched: true },
+      isEmailInDb: true,
+    });
+  }
+
+  updatePassword(password) {
+    this.setState({
+      password: { value: password, touched: true },
+      isPasswordCorrect: true,
+    });
+  }
+
+  validateEmail() {
+    const email = this.state.email.value.trim();
+    if (email.length === 0) {
+      return "Email is required";
+    } else if (!validator.isEmail(email)) {
+      return "Please enter a valid email address";
+    }
+  }
+
+  validatePassword() {
+    const password = this.state.password.value.trim();
+    if (password.length === 0) {
+      return "Password is required";
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Potentially offer a forgot password link, that will link to security questions
+  }
+
+  render() {
+    return (
+      <>
+        <div className="sign-on-container">
+          <Link to="/">
+            <h2 id="heading-sign-on">Credit Card Recommender</h2>
+          </Link>
+          <div id="form-container">
+            <div id="form-section">
+              <form
+                className="signup-form"
+                onSubmit={(e) => {
+                  this.handleSubmit(e);
+                }}
+              >
+                <div className="form-group">
+                  <div className="form-field" id="email">
+                    <div>
+                      <i className="fa fa-user"></i>
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      onChange={(e) => this.updateEmail(e.target.value)}
+                    />
+                  </div>
+                  <small className="error">
+                    {this.state.email.touched && (
+                      <ValidationError message={this.validateEmail()} />
+                    )}
+                    {!this.state.isEmailInDb && (
+                      <ValidationError message="The email entered is not associated with any account" />
+                    )}
+                  </small>
+                  <div className="form-field" id="password">
+                    <div>
+                      <i className="fa fa-lock"></i>
+                    </div>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      onChange={(e) => this.updatePassword(e.target.value)}
+                    />
+                  </div>
+                  <small className="error">
+                    {this.state.password.touched && (
+                      <ValidationError message={this.validatePassword()} />
+                    )}
+                    {!this.state.isPasswordCorrect && (
+                      <ValidationError message="The password entered is incorrect" />
+                    )}
+                  </small>
+                </div>
+                <button
+                  type="submit"
+                  id="submit-btn"
+                  disabled={this.validateEmail() || this.validatePassword()}
+                >
+                  Sign In
+                </button>
+                <div id="register-text">
+                  <small>
+                    Don't have an account?
+                    <Link to="/sign-up">
+                      <span>Register</span>
+                    </Link>
+                  </small>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
