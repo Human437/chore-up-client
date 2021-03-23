184d2a6441b18c278d675676e4d4d54e1d4940fd
Ryan <leeyuenhigh@gmail.com>
2021-03-23 19:21:53 -0400

    Updated management page with buttons and code to join family

--- src/management/management.js
+++ src/management/management.js
@@ -18,19 +18,22 @@ export default class Management extends React.Component {
           {/* Only admins of the family should be able to see this page and manage users
           Admins should be able to remove users */}
           <ul>
-            <li>Jack</li>
-            <li>Jill</li>
-            <li>Bob</li>
-            <li>Jane</li>
-            <li>Mary</li>
+            <li>Jack <button>Kick</button></li>
+            <li>Jill <button>Kick</button></li>
+            <li>Bob <button>Kick</button></li>
+            <li>Jane <button>Kick</button></li>
+            <li>Mary <button>Kick</button></li>
           </ul>
           <h2>Chores to be approved</h2>
           {/* Only admins should be able to approve chores that were completed */}
           <ul>
-            <li>Walking the dog</li>
-            <li>Washing the dishes</li>
-            <li>Buying groceries</li>
+            <li>Walking the dog <button>Mark as Approved</button><button>Mark as Redo</button></li>
+            <li>Washing the dishes <button>Mark as Approved</button><button>Mark as Redo</button></li>
+            <li>Buying groceries <button>Mark as Approved</button><button>Mark as Redo</button></li>
           </ul>
+          <h2>Code to join family</h2>
+          <h4>Current Code: ahw3j</h4>
+          <button>Regenerate code</button>
         </main>
         <footer role="content-info">Footer</footer>
       </>
