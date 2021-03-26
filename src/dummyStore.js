06270d50e58da809bd20e36084515817810b0495
Ryan <leeyuenhigh@gmail.com>
2021-03-26 02:41:17 -0400

    Reference users by id and added chores table

--- src/dummyStore.js
+++ src/dummyStore.js
@@ -3,13 +3,13 @@ export default {
     {
       "id" : 1,
       "admin":"Bob",
-      "members":["Bob","Jack","Jill"],
+      "members":[1,2,3],
       "code-2-join":"aJk3T"
     },
     {
       "id" : 2,
       "admin":"Mary",
-      "members":["Mary","John","Amy"],
+      "members":[4,5,6],
       "code-2-join":"bJk3T"
     },
   ],
@@ -17,11 +17,7 @@ export default {
     {
       "id":1,
       "name":"Bob",
-      "chores":[
-        {"Walk the dog":10},
-        {"Take Jack to school":20},
-        {"Go to the PO box":20}
-      ],
+      "chores":[1,2,3],
       "level":1,
       "xp-till-level-up":100,
       "email":"hwpyxoutfugfqbusvz@twzhhq.com",
@@ -30,10 +26,7 @@ export default {
     {
       "id":2,
       "name":"Jack",
-      "chores":[
-        {"Buy groceries":20},
-        {"Water the plants":10}
-      ],
+      "chores":[4,5],
       "level":1,
       "xp-till-level-up":100,
       "email":"hwpyxoutfugfqbusvz@twzhhq.com",
@@ -42,10 +35,7 @@ export default {
     {
       "id":3,
       "name":"Jill",
-      "chores":[
-        {"Do the laundry":10},
-        {"Take out the trash":10}
-      ],
+      "chores":[6,7],
       "level":1,
       "xp-till-level-up":100,
       "email":"hwpyxoutfugfqbusvz@twzhhq.com",
@@ -54,10 +44,7 @@ export default {
     {
       "id":4,
       "name":"Mary",
-      "chores":[
-        {"Do the laundry":10},
-        {"Take out the trash":10}
-      ],
+      "chores":[8,9],
       "level":1,
       "xp-till-level-up":100,
       "email":"hwpyxoutfugfqbusvz@twzhhq.com",
@@ -66,10 +53,7 @@ export default {
     {
       "id":5,
       "name":"John",
-      "chores":[
-        {"Feed the fish":10},
-        {"Vacuum the carpet":30}
-      ],
+      "chores":[10,11],
       "level":1,
       "xp-till-level-up":100,
       "email":"hwpyxoutfugfqbusvz@twzhhq.com",
@@ -78,14 +62,118 @@ export default {
     {
       "id":6,
       "name":"Amy",
-      "chores":[
-        {"Water the plants":10},
-        {"Clean the fridge":30}
-      ],
+      "chores":[12,13],
       "level":1,
       "xp-till-level-up":100,
       "email":"hwpyxoutfugfqbusvz@twzhhq.com",
       "hashedPassword":"$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C",
+    },
+  ],
+  'chores':[
+    {
+      "id":1,
+      "name":"Walk the dog",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":2,
+      "name":"Take Jack to school",
+      "value":20,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":3,
+      "name":"Go to the PO box",
+      "value":20,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":4,
+      "name":"Buy groceries",
+      "value":20,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":5,
+      "name":"Water the plants",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":6,
+      "name":"Do the laundry",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":7,
+      "name":"Take out the trash",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":8,
+      "name":"Do the laundry",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":9,
+      "name":"Take out the trash",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":10,
+      "name":"Do the laundry",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":11,
+      "name":"Take out the trash",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":12,
+      "name":"Feed the fish",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":13,
+      "name":"Vacuum the carpet",
+      "value":30,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":14,
+      "name":"Water the plants",
+      "value":10,
+      "status":"Incomplete",
+      "comments":""
+    },
+    {
+      "id":15,
+      "name":"Clean the fridge",
+      "value":30,
+      "status":"Incomplete",
+      "comments":""
     },
   ]
 }
