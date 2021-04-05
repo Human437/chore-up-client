04b377fcb54ee65be9a330e1cf515f11c4f97086
Ryan <leeyuenhigh@gmail.com>
2021-04-05 17:11:19 -0400

    Imported join-or-create-family, added route for it, and updated stuff being passed through context

--- src/App.js
+++ src/App.js
@@ -10,16 +10,41 @@ import SignUp from "./sign-up/sign-up";
 import NavBar from "./navbar/navbar";
 import HomePage from "./home-page/home-page";
 import MyLevel from "./my-level/my-level";
+import JoinOrCreateFamily from './join-or-create-family/join-or-create-family'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isSignedIn: false,
+      isAdmin: false,
+      userId:null,
+    };
+  }
+
+  updateIsSignedIn = (state) => {
+    this.setState({ isSignedIn: state });
+  };
+
+  updateUserId = (id) => {
+    this.setState({ userId: id });
+  };
+
+  updateIsAdmin = (state) => {
+    this.setState({isAdmin: state})
   }
 
   render() {
     return (
-      <ChoreUpContext.Provider value={{}}>
+      <ChoreUpContext.Provider 
+      value={{
+        isSignedIn: this.state.isSignedIn,
+        isAdmin: this.state.isAdmin,
+        userId: this.state.userId,
+        updateIsSignedIn: this.updateIsSignedIn,
+        updateIsAdmin: this.updateIsAdmin,
+        updateUserId: this.updateUserId,
+      }}>
         <main>
           <Route path="/" component={NavBar} />
           <Route exact path="/" component={HomePage} />
@@ -29,6 +54,7 @@ export default class App extends React.Component {
           <Route path="/sign-in" component={SignIn} />
           <Route path="/sign-up" component={SignUp} />
           <Route path="/my-level" component={MyLevel} />
+          <Route path="/join-or-create-family" component={JoinOrCreateFamily}/>
         </main>
       </ChoreUpContext.Provider>
     );
