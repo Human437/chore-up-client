32a1096cbcaeccade7da1b26d7e4235043cef771
Ryan <leeyuenhigh@gmail.com>
2021-04-05 19:28:49 -0400

    Wired button to push join family page when join family button is pushed

--- src/join-or-create-family/join-or-create-family.js
+++ src/join-or-create-family/join-or-create-family.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import "./join-or-create-family.css"
+import React from "react";
+import "./join-or-create-family.css";
 
 export default class JoinOrCreateFamily extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
         <h1>Join or Create a Family</h1>
-        <button>Create a Family</button>
-        <br/>
-        <button>Join a Family</button>
+        <button 
+          id="create-a-family-btn"
+        >
+          Create a Family
+        </button>
+        <br />
+        <button
+          id="join-a-family-btn"
+          onClick={(e) => this.props.history.push('/join-family')}
+        >
+          Join a Family
+        </button>
       </>
-    )
+    );
   }
 }
