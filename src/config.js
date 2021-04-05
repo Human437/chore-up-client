45ef9d3e0365f9c23c261d056dbae2f277d9b94f
Ryan <leeyuenhigh@gmail.com>
2021-04-05 07:16:28 -0400

    Updated config with corresponding endpoints for choreup

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+export default {
+  BEARER_TOKEN: process.env.REACT_APP_BEARER_TOKEN,
+  API_Chores_Endpoint: process.env.REACT_APP_API_Chores_Endpoint,
+  API_Families_Enpoint:process.env.REACT_APP_API_Families_Enpoint,
+  API_Users_Endpoint:process.env.REACT_APP_API_Users_Endpoint,
+  API_User_Chores_Endpoint:process.env.REACT_APP_API_User_Chores_Endpoint,
+  API_Family_Members_Endpoint:process.env.REACT_APP_API_Family_Members_Endpoint
+}
