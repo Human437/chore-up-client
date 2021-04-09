0860212ed197db11b9bf396f226320887d2c7286
Ryan <leeyuenhigh@gmail.com>
2021-04-09 03:15:18 -0400

    Gets a specified user's chores and lists them out

--- src/my-chores/my-chores.js
+++ src/my-chores/my-chores.js
@@ -1,27 +1,48 @@
 import React from 'react'
 import "./my-chores.css"
-import {Link} from "react-router-dom"
+import config from "./../config"
 
 export default class MyChores extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      userChoresArray:[],
     }
+  }
+
+  getUserChores(){
+    fetch(`${config.API_User_Chores_Endpoint}/user/${this.props.match.params.id}`,{
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${config.BEARER_TOKEN}`,
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({userChoresArray:data})
+    })
+  }
+
+  componentDidMount(){
+    this.getUserChores()
   }
 
   render(){
     return(
       <>
         <main role="main">
-          <h2>Chores</h2>
-          <ul>
-            <li>Washing the dishes <button>Mark As Done</button></li>
-            <li>Making dinner <button>Mark As Done</button></li>
-            <li>Vacuum the carpet <button>Mark As Done</button></li>
-          </ul>
+          <h1>Assigned Chores</h1>
+          {this.state.userChoresArray.map((chore,index) => {
+            return (
+              <div key={chore.chore_id} className='chores'>
+                <h3>Task: {chore.name}</h3>
+                <h4>Value: {chore.value}</h4>
+                <p>Comments: {chore.comments}</p>
+                <button>Mark As Done</button>
+              </div>
+            )
+          })}
         </main>
-        <footer role="content-info">Footer</footer>
       </>
     )
   }
