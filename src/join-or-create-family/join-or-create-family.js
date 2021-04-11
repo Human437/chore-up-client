9da4eefffdd9839b6d3b1f0583e5ccbf8f2d0c3b
Ryan <leeyuenhigh@gmail.com>
2021-04-11 03:51:44 -0400

    Added handleCreateFamilyBtn to make the user a admin if they choose to make a family

--- src/join-or-create-family/join-or-create-family.js
+++ src/join-or-create-family/join-or-create-family.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./join-or-create-family.css";
+import ChoreUpContext from './../choreUpContext'
+import config from './../config'
 
 export default class JoinOrCreateFamily extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  static contextType = ChoreUpContext
+
+  handleCreateFamilyBtn(){
+    this.context.updateIsAdmin(true)
+    fetch(`${config.API_Users_Endpoint}/${this.context.userId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        is_admin: true
+      }),
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${config.BEARER_TOKEN}`,
+      },
+    })
+    this.props.history.push('/create-family')
   }
 
   render() {
@@ -13,14 +32,14 @@ export default class JoinOrCreateFamily extends React.Component {
         <h1>Join or Create a Family</h1>
         <button 
           id="create-a-family-btn"
-          onClick={(e) => this.props.history.push('/create-family')}
+          onClick={() => this.handleCreateFamilyBtn()}
         >
           Create a Family
         </button>
         <br />
         <button
           id="join-a-family-btn"
-          onClick={(e) => this.props.history.push('/join-family')}
+          onClick={() => this.props.history.push('/join-family')}
         >
           Join a Family
         </button>
