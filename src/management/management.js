4ea1a92e41cf817e6a9bfcb91dc955f4f132111a
Ryan <leeyuenhigh@gmail.com>
2021-04-11 01:59:58 -0400

    Added conditional rendering so that only a signed in admin can view the family management page

--- src/management/management.js
+++ src/management/management.js
@@ -76,42 +76,52 @@ export default class Management extends React.Component {
   }
 
   componentDidMount(){
-    this.getFamilyMembers()
-    this.getFamilyCode()
+    if(this.context.isSignedIn && this.context.isAdmin){
+      this.getFamilyMembers()
+      this.getFamilyCode()
+    }
   }
 
   render(){
-    return(
-      <>
-        <main role="main">
-          <h1>Family Management</h1>
-          <h2>Members</h2>
-          {/* Only admins of the family should be able to see this page and manage users
-          Admins should be able to remove users */}
-          <div id="members">
-            {this.state.familyMembers.map((member,index) => {
-              // Doesn't show the admin family member as a member so the admin can't kick themself
-              if(!member.is_admin){
-                return (
-                  <div key={member.user_id} className="family_member">
-                    <h4>{member.name}</h4>
-                    <button
-                      data-index={index}
-                      data-family_member={member.id}
-                      onClick={(e) =>{this.handleKickMember(e)}}
-                    >
-                      Kick
-                    </button>
-                  </div>
-                )
-              }
-            })}
-          </div>
-          <h2>Code to join family</h2>
-          <h4>Current Code: {this.state.familyCode}</h4>
-        </main>
-        <footer role="content-info">Footer</footer>
-      </>
-    )
+    if(this.context.isSignedIn && this.context.isAdmin){
+      return(
+        <>
+          <main role="main">
+            <h1>Family Management</h1>
+            <h2>Members</h2>
+            {/* Only admins of the family should be able to see this page and manage users
+            Admins should be able to remove users */}
+            <div id="members">
+              {this.state.familyMembers.map((member,index) => {
+                // Doesn't show the admin family member as a member so the admin can't kick themself
+                if(!member.is_admin){
+                  return (
+                    <div key={member.user_id} className="family_member">
+                      <h4>{member.name}</h4>
+                      <button
+                        data-index={index}
+                        data-family_member={member.id}
+                        onClick={(e) =>{this.handleKickMember(e)}}
+                      >
+                        Kick
+                      </button>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+            <h2>Code to join family</h2>
+            <h4>Current Code: {this.state.familyCode}</h4>
+          </main>
+          <footer role="content-info">Footer</footer>
+        </>
+      )
+    }else{
+      return (
+        <>
+          <h1>You must be signed in as a admin to view the family management page</h1>
+        </>
+      )
+    }
   }
 }
