60a43ba488a3647aaca61456ec15baae0119c7ce
Ryan <leeyuenhigh@gmail.com>
2021-04-11 02:13:20 -0400

    Conditionally renders the nav bar based on the type of user signed in and if the user is signed in

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./navbar.css"
 import {Link} from "react-router-dom"
+import ChoreUpContext from './../choreUpContext'
 
 export default class NavBar extends React.Component {
   constructor(props){
@@ -10,19 +11,43 @@ export default class NavBar extends React.Component {
     }
   }
 
+  static contextType = ChoreUpContext
+
   render(){
-    return(
-      <>
-        <nav role="navigation">
-          <Link to="/">Home</Link>
-          <Link to= '/my-chores'>My Chores</Link>
-          <Link to= '/create-a-chore'>Create a Chore</Link>
-          <Link to= '/management'>Management</Link>
-          <Link to= '/my-level'>My Level</Link>
-          <button><Link to="/sign-in">Sign In</Link></button>
-          <button><Link to="/sign-up">Sign Up</Link></button>
-        </nav>
-      </>
-    )
+    if(this.context.isSignedIn && this.context.isAdmin){
+      return(
+        <>
+          <nav role="navigation">
+            <Link to="/">Home</Link>
+            <Link to= {`/my-chores/${this.context.userId}`}>My Chores</Link>
+            <Link to= '/create-a-chore'>Create a Chore</Link>
+            <Link to= '/management'>Management</Link>
+            <Link to= {`/my-profile/${this.context.userId}`}>My Profile</Link>
+            <button><Link to="/">Sign Out</Link></button>
+          </nav>
+        </>
+      )
+    }else if(this.context.isSignedIn){
+      return(
+        <>
+          <nav role="navigation">
+            <Link to="/">Home</Link>
+            <Link to= {`/my-chores/${this.context.userId}`}>My Chores</Link>
+            <Link to= {`/my-profile/${this.context.userId}`}>My Profile</Link>
+            <button><Link to="/">Sign Out</Link></button>
+          </nav>
+        </>
+      )
+    }else{
+      return(
+        <>
+          <nav role="navigation">
+            <Link to="/">Home</Link>
+            <button><Link to="/sign-in">Sign In</Link></button>
+            <button><Link to="/sign-up">Sign Up</Link></button>
+          </nav>
+        </>
+      )
+    }
   }
 }
