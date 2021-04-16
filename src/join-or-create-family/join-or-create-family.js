c0c6a57dfa6bd2645ae31771897bfcd67a4013c8
Ryan <leeyuenhigh@gmail.com>
2021-04-16 03:17:57 -0400

    Added ids and used prettier to format code

--- src/join-or-create-family/join-or-create-family.js
+++ src/join-or-create-family/join-or-create-family.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./join-or-create-family.css";
-import ChoreUpContext from './../choreUpContext'
-import config from './../config'
+import ChoreUpContext from "./../choreUpContext";
+import config from "./../config";
 
 export default class JoinOrCreateFamily extends React.Component {
   constructor(props) {
@@ -9,40 +9,42 @@ export default class JoinOrCreateFamily extends React.Component {
     this.state = {};
   }
 
-  static contextType = ChoreUpContext
+  static contextType = ChoreUpContext;
 
-  handleCreateFamilyBtn(){
-    this.context.updateIsAdmin(true)
+  handleCreateFamilyBtn() {
+    this.context.updateIsAdmin(true);
     fetch(`${config.API_Users_Endpoint}/${this.context.userId}`, {
       method: "PATCH",
       body: JSON.stringify({
-        is_admin: true
+        is_admin: true,
       }),
       headers: {
         "content-type": "application/json",
         Authorization: `Bearer ${config.BEARER_TOKEN}`,
       },
-    })
-    this.props.history.push('/create-family')
+    });
+    this.props.history.push("/create-family");
   }
 
   render() {
     return (
       <>
-        <h1>Join or Create a Family</h1>
-        <button 
-          id="create-a-family-btn"
-          onClick={() => this.handleCreateFamilyBtn()}
-        >
-          Create a Family
-        </button>
-        <br />
-        <button
-          id="join-a-family-btn"
-          onClick={() => this.props.history.push('/join-family')}
-        >
-          Join a Family
-        </button>
+        <main id="join-create-family-page">
+          <h1 id="join-create-family-header">Join or Create a Family</h1>
+          <button
+            id="create-a-family-btn"
+            onClick={() => this.handleCreateFamilyBtn()}
+          >
+            Create a Family
+          </button>
+          <br />
+          <button
+            id="join-a-family-btn"
+            onClick={() => this.props.history.push("/join-family")}
+          >
+            Join a Family
+          </button>
+        </main>
       </>
     );
   }
