c19d737797f829a4b77cadca7fe38f761459de0c
Ryan <leeyuenhigh@gmail.com>
2021-04-16 00:01:29 -0400

    Added additional id tags and formatted code using prettier

--- src/home-page/home-page.js
+++ src/home-page/home-page.js
@@ -1,30 +1,26 @@
-import React from 'react'
-import "./home-page.css"
+import React from "react";
+import "./home-page.css";
 
 export default class HomePage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
-        <main role="main">
+        <main role="main" id="landing-page">
           <header role="banner">
-            <h1>Chore Up</h1>
+            <h1 id="landing-page-h1">Chore Up</h1>
           </header>
           <section>
-            <header>
-                <h3>Keeping track of chores</h3>
+            <header id="landing-page-h2">
+              <h2>Keeping track of chores, so you don't have to</h2>
             </header>
-            <p>Know exactly who is assigned what chore and the status of the chore</p>
           </section>
         </main>
-        <footer role="content-info">Footer</footer>
       </>
-    )
+    );
   }
 }
