62e8e7efcbe80b55f90c65b1af12b7c4bfa1984e
Ryan <leeyuenhigh@gmail.com>
2021-04-16 01:58:31 -0400

    Added more sections for css and formatted using prettier

--- src/management/management.js
+++ src/management/management.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import "./management.css"
-import config from './../config'
-import ChoreUpContext from './../choreUpContext'
+import React from "react";
+import "./management.css";
+import config from "./../config";
+import ChoreUpContext from "./../choreUpContext";
 
 export default class Management extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      familyMembers:[],
-      familyCode:"not-set"
-    }
+      familyMembers: [],
+      familyCode: "not-set",
+    };
   }
 
-  static contextType = ChoreUpContext
+  static contextType = ChoreUpContext;
 
-  getFamilyMembers(){
+  getFamilyMembers() {
     fetch(`${config.API_Family_Members_Endpoint}/user/${this.context.userId}`, {
       method: "GET",
       headers: {
@@ -22,36 +22,39 @@ export default class Management extends React.Component {
       },
     })
       .then((response) => response.json())
-      .then((data) =>{
-        fetch(`${config.API_Family_Members_Endpoint}/family/${data.family_id}`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${config.BEARER_TOKEN}`,
-          },
-        })
+      .then((data) => {
+        fetch(
+          `${config.API_Family_Members_Endpoint}/family/${data.family_id}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${config.BEARER_TOKEN}`,
+            },
+          }
+        )
           .then((response) => response.json())
           .then((data) => {
-            this.setState({familyMembers:data})
-          })
-      })
+            this.setState({ familyMembers: data });
+          });
+      });
   }
 
-  handleKickMember(e){
-    const family_memberId = e.target.dataset.family_member
-    const index = e.target.dataset.index
-    let familyMembers = this.state.familyMembers
-    familyMembers.splice(index,1)
+  handleKickMember(e) {
+    const family_memberId = e.target.dataset.family_member;
+    const index = e.target.dataset.index;
+    let familyMembers = this.state.familyMembers;
+    familyMembers.splice(index, 1);
     //uses e.target.dataset to access custom html attributes
-    this.setState({familyMembers:familyMembers})
-    fetch(`${config.API_Family_Members_Endpoint}/${family_memberId}`,{
-      method:"DELETE",
+    this.setState({ familyMembers: familyMembers });
+    fetch(`${config.API_Family_Members_Endpoint}/${family_memberId}`, {
+      method: "DELETE",
       headers: {
         Authorization: `Bearer ${config.BEARER_TOKEN}`,
       },
-    })
+    });
   }
 
-  getFamilyCode(){
+  getFamilyCode() {
     fetch(`${config.API_Family_Members_Endpoint}/user/${this.context.userId}`, {
       method: "GET",
       headers: {
@@ -59,7 +62,7 @@ export default class Management extends React.Component {
       },
     })
       .then((response) => response.json())
-      .then((data) =>{ 
+      .then((data) => {
         fetch(`${config.API_Families_Enpoint}/${data.family_id}`, {
           method: "GET",
           headers: {
@@ -68,58 +71,73 @@ export default class Management extends React.Component {
         })
           .then((response) => response.json())
           .then((data) => {
-            this.setState({familyCode:data.code_to_join})
-          })
-      })
+            this.setState({ familyCode: data.code_to_join });
+          });
+      });
   }
 
-  componentDidMount(){
-    if(this.context.isSignedIn && this.context.isAdmin){
-      this.getFamilyMembers()
-      this.getFamilyCode()
+  componentDidMount() {
+    if (this.context.isSignedIn && this.context.isAdmin) {
+      this.getFamilyMembers();
+      this.getFamilyCode();
     }
   }
 
-  render(){
-    if(this.context.isSignedIn && this.context.isAdmin){
-      return(
-        <>
-          <main role="main">
-            <h1>Family Management</h1>
-            <h2>Members</h2>
-            {/* Only admins of the family should be able to see this page and manage users
-            Admins should be able to remove users */}
-            <div id="members">
-              {this.state.familyMembers.map((member,index) => {
-                // Doesn't show the admin family member as a member so the admin can't kick themself
-                if(!member.is_admin){
-                  return (
-                    <div key={member.user_id} className="family_member">
-                      <h4>{member.name}</h4>
-                      <button
-                        data-index={index}
-                        data-family_member={member.id}
-                        onClick={(e) =>{this.handleKickMember(e)}}
-                      >
-                        Kick
-                      </button>
-                    </div>
-                  )
-                }
-              })}
-            </div>
-            <h2>Code to join family</h2>
-            <h4>Current Code: {this.state.familyCode}</h4>
-          </main>
-          <footer role="content-info">Footer</footer>
-        </>
-      )
-    }else{
+  render() {
+    if (this.context.isSignedIn && this.context.isAdmin) {
       return (
         <>
-          <h1>You must be signed in as a admin to view the family management page</h1>
+          <header>
+            <h1 id="management-page-header">Family Management</h1>
+          </header>
+          <main role="main" id="management-page">
+            <div className="management-item">
+              <h2>Members</h2>
+              {/* Only admins of the family should be able to see this page and manage users
+              Admins should be able to remove users */}
+              <div id="members">
+                {this.state.familyMembers.map((member, index) => {
+                  // Doesn't show the admin family member as a member so the admin can't kick themself
+                  if (!member.is_admin) {
+                    return (
+                      <div key={member.user_id} className="family_member">
+                        <div className="member-info">
+                          <h4>Name: {member.name}</h4>
+                          <h4>Level: {member.level}</h4>
+                          <h4>XP Till Level Up: {member.xp_till_level_up}</h4>
+                          <h4>Email: {member.email}</h4>
+                        </div>
+                        <button
+                          data-index={index}
+                          data-family_member={member.id}
+                          onClick={(e) => {
+                            this.handleKickMember(e);
+                          }}
+                          className="kick-btn"
+                        >
+                          Kick
+                        </button>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            </div>
+            <div className="management-item">
+              <h2>Code to join family</h2>
+              <h4 id="family-code">{this.state.familyCode}</h4>
+            </div>
+          </main>
         </>
-      )
+      );
+    } else {
+      return (
+        <>
+          <h1>
+            You must be signed in as a admin to view the family management page
+          </h1>
+        </>
+      );
     }
   }
 }
